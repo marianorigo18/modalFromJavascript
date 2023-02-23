456bf9717b740b69e1f3227a4fd4ea5c4e31efe5
marianorigo18 <marianowagnerrigo@gmail.com>
2023-02-23 10:29:59 -0300

    modalCalculator ok

--- app.js
+++ app.js
@@ -1,33 +1,76 @@
 const boxContent = document.querySelector('.box-container');
-const containerModal = document.querySelector('.container-modal');
-const containerModalCalculator = document.querySelector('.container-modal-calculator');
+const main = document.querySelector('main');
+
+function obtenerModalCalculator(){
+    const containerModalCalculator = document.querySelector('.container-modal-calculator');
+    containerModalCalculator.addEventListener('click', (e) => {
+        const btnCloseModal = e.target.classList.contains('btn-close-calculator');
+        const btnCloseModalTwo = e.target.classList.contains('btn-close-two-calculator');
+        const fueraDelModalTwo = e.target.classList.contains('container-modal-calculator');
+        if(btnCloseModalTwo || btnCloseModal || fueraDelModalTwo){
+            containerModalCalculator.remove()
+        }
+    })
+}
+
+function renderModalCalculator(){
+    const cModalCalculator = document.createElement('div');
+    cModalCalculator.classList = 'container-modal-calculator open-modal-calculator';
+    cModalCalculator.innerHTML = `
+    <div class="modal-content-calculator">
+    <span class="btn-close-calculator">X</span>
+    <div class="modal-content-img-calculator">
+        <img src="https://img.ecologiahoy.com/2017/06/paisajes-naturales-26.jpeg" alt="">
+    </div>
+    <div class="modal-body-calculator">
+        <button class="btn-close-two-calculator">close</button>
+    </div>
+    </div>
+    `
+    main.appendChild(cModalCalculator)
+    obtenerModalCalculator();
+}
+
+function obtenerModal(){
+    const containerModal = document.querySelector('.container-modal');
+    containerModal.addEventListener('click', (e) => {
+        const btnClose = e.target.classList.contains('btn-close');        
+        const fueraDelModal = e.target.classList.contains('container-modal');
+        const btnCloseTwo = e.target.classList.contains('btn-close-two');
+        /*other modal*/
+        const btnOpenOthelModal = e.target.classList.contains('btn-open-other-modal');
+        if(btnClose || fueraDelModal || btnCloseTwo){
+            return containerModal.remove()
+        }
+        if(btnOpenOthelModal){
+           renderModalCalculator()
+        }
+    })
+
+}
+
+function rederModal(){
+    const cModal = document.createElement('div');
+    cModal.classList = 'container-modal open-modal';
+    cModal.innerHTML = `
+    <div class="modal-content">
+    <span class="btn-close">X</span>
+    <div class="modal-content-img">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrbsyUiqBR1DDVM4-lU-Kmz1vSrEGoteHUW-fageuH&s" alt="">
+    </div>
+    <div class="modal-body">
+        <button class="btn-close-two">close</button>
+        <button class="btn-open-other-modal">open other modal</button>
+    </div>
+    </div>
+    `
+    main.appendChild(cModal)
+    obtenerModal()
+}
 
 boxContent.addEventListener('click', (e)=> {
     const cuadrado = e.target.classList.contains('cuadrado');
     if(cuadrado){
-        containerModal.classList.add('open-modal')
+        rederModal()
     }
 });
-
-containerModal.addEventListener('click', (e) => {
-    const btnClose = e.target.classList.contains('btn-close');
-    const fueraDelModal = e.target.classList.contains('container-modal');
-    const btnCloseTwo = e.target.classList.contains('btn-close-two');
-    /*other modal*/
-    const btnOpenOthelModal = e.target.classList.contains('btn-open-other-modal');
-    if(btnClose || fueraDelModal || btnCloseTwo){
-        return containerModal.classList.remove('open-modal')
-    }
-    if(btnOpenOthelModal){
-        containerModalCalculator.classList.add('open-modal-calculator')
-    }
-})
-
-containerModalCalculator.addEventListener('click', (e) => {
-    const btnCloseModal = e.target.classList.contains('btn-close-calculator');
-    const btnCloseModalTwo = e.target.classList.contains('btn-close-two-calculator');
-    const fueraDelModalTwo = e.target.classList.contains('container-modal-calculator');
-    if(btnCloseModalTwo || btnCloseModal || fueraDelModalTwo){
-        containerModalCalculator.classList.remove('open-modal-calculator')
-    }
-})
